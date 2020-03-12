710dc182a1e5005379506641b2fb56082d4cc140
luxjoshyua <josh.e.fielding@gmail.com>
2020-03-13 07:55:53 +1100

    started js

--- src/index.js
+++ src/index.js
@@ -1,1 +1,15 @@
 import style from "./sass/index.scss";
+
+
+const bookmarkingLink = document.getElementById('bookmarking');
+const speedyLink = document.getElementById('speedy');
+const sharingLink = document.getElementById('sharing');
+const itemTwo = document.querySelector('.item__two');
+
+
+
+speedyLink.addEventListener('click', (e) => {
+    bookmarkingLink.classList.remove('active');
+    speedyLink.classList.toggle('active');
+    itemTwo.classList.toggle('active');
+});
