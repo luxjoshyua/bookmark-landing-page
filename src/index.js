eb37e9595ffdc401ce6f60367154676e44f4b06c
luxjoshyua <josh.e.fielding@gmail.com>
2020-03-14 08:58:32 +1100

    started extensions section

--- src/index.js
+++ src/index.js
@@ -4,12 +4,37 @@ import style from "./sass/index.scss";
 const bookmarkingLink = document.getElementById('bookmarking');
 const speedyLink = document.getElementById('speedy');
 const sharingLink = document.getElementById('sharing');
+
+const itemOne = document.querySelector('.item__one');
 const itemTwo = document.querySelector('.item__two');
+const itemThree = document.querySelector('.item__three');
 
+bookmarkingLink.addEventListener('click', e => {
+    bookmarkingLink.classList.toggle('active');
+    speedyLink.classList.remove('active');
+    sharingLink.classList.remove('active');
 
+    itemOne.classList.toggle('active');
+    itemTwo.classList.remove('active');
+    itemThree.classList.remove('active');
+});
 
-speedyLink.addEventListener('click', (e) => {
+speedyLink.addEventListener('click', e => {
     bookmarkingLink.classList.remove('active');
     speedyLink.classList.toggle('active');
+    sharingLink.classList.remove('active');
+
+    itemOne.classList.remove('active');
     itemTwo.classList.toggle('active');
+    itemThree.classList.remove('active');
+});
+
+sharingLink.addEventListener('click', e => {
+    bookmarkingLink.classList.remove('active');
+    speedyLink.classList.remove('active');
+    sharingLink.classList.toggle('active');
+
+    itemOne.classList.remove('active');
+    itemTwo.classList.remove('active');
+    itemThree.classList.toggle('active');
 });
